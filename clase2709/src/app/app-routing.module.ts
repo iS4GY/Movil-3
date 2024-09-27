6634aef81c3b0786fafa15c03d9df5243bda47f8
JPreus11 <juan.pedro.leon11@gmail.com>
2024-09-27 11:27:38 -0300

    JPreus11
    aaa

--- clase2709/src/app/app-routing.module.ts
+++ clase2709/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'not-found',
+    loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
