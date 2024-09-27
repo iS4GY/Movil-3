e4d473765a6ec8e6f4ad63c0a4bcfbc04463d39b
JPreus11 <juan.pedro.leon11@gmail.com>
2024-09-27 11:30:07 -0300

    DNJ
    ADKFMFD

--- clase2709/src/app/auth.service.ts
+++ clase2709/src/app/auth.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
-}
+  private isAuthenticated = false; // Simula si el usuario está autenticado
+  login(username: string, password: string): boolean {
+  // Simulamos una validación de login
+  if (username === 'usuario' && password === '1234') {
+  this.isAuthenticated = true;
+  return true;
+  }
+  return false;
+  }
+  isLoggedIn(): boolean {
+  return this.isAuthenticated;
+  }
+  logout() {
+  this.isAuthenticated = false;
+  }
+  }
--- clase2709/src/app/login/login.page.ts
+++ clase2709/src/app/login/login.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+selector: 'app-login',
+templateUrl: './login.page.html',
+styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginPage {
+username: string = '';
+password: string = '';
+constructor(private authService: AuthService, private router: Router) {}
+login() {
+if (this.authService.login(this.username, this.password)) {
+//aprovechamos de usar state para llevar la informacion al dashboard.
+this.router.navigate(['/dashboard'], { state: { username: this.username } });
+} else {
+alert('Nombre de usuario o contraseña incorrectos');
   }
-
+}
 }
